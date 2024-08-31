8787b18ef5ed8961e494ba2557a35a85c0330ed6
Prabin Karki <karkiprabin198@gmail.com>
2024-08-31 20:10:16 +0545

    Analog Clock

---
+++ script.js
@@ -0,0 +1,17 @@
+const hour=document.getElementById('hour');
+const minute=document.getElementById('minute');
+const second=document.getElementById('second');
+
+setInterval(()=> {
+  const  d = new Date();
+   let hTime=d.getHours();
+   let mTime=d.getMinutes();
+   let sTime=d.getSeconds();
+   let hRotation=30*hTime+mTime/2;
+   let mRotation=6*mTime;
+   let sRotation=6*sTime;
+  
+    hour.style.transform= `rotate(${hRotation}deg)`
+    minute.style.transform= `rotate(${mRotation}deg)`
+    second.style.transform= `rotate(${sRotation}deg)`
+},1000);
